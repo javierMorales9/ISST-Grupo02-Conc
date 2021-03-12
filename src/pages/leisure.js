5547b6c306365750cba49ca454d3b92b8724fb97
acallejasz <5aadrianc@gmail.com>
2021-03-12 20:13:04 +0100

    router

---
+++ src/pages/leisure.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route,Link} from "react-router-dom";
+
+export default class Game extends React.Component {
+    render() {
+        return (
+          <div>
+              <h1>Leisure</h1>
+          </div>
+        );
+    }
+}
