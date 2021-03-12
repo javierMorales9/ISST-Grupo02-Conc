fafce87a465ca47d25420b07c1eadd58d3984668
Javier Morales <javiermoralesdevera@gmail.com>
2021-03-12 18:05:22 +0100

    react_inicio

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import './App.css';
+import { connect } from 'react-redux';
+import Inicio from './Inicio'
+
+
+function App(props) {
+  return (
+    <div className="App">
+        <Inicio/>
+    </div>
+  );
+}
+
+function mapStateToProps(state) {
+return {
+...state
+};
+}
+export default connect(mapStateToProps)(App);
---
+++ src/estilos_inicio.js
@@ -0,0 +1,296 @@
+import React from 'react';
+
+// PARA EL FOOTER
+
+export const footer={
+	backgroundColor: 'rgba(81, 53, 27, 0.82)',
+	width: '100%',
+	display: 'flex',
+	flexDirection: 'row',
+	alignItems: 'center',
+	justifyContent: 'center',
+	left: '0',
+	right: '0',
+	padding: '0',
+	margin:'0 0 0'
+}
+
+
+export const footer_upper_text={
+	fontFamily: 'Ibarra Real Nova',
+	fontStyle: 'normal',
+	fontWeight: 'normal',
+	fontSize: '55px',
+	lineHeight: '137px',
+	display: 'flex',
+	alignItems: 'center',
+	textAlign: 'center',
+	color: '#FFFFFF'
+}
+
+export const footer_lower_text={
+	fontFamily: 'Ibarra Real Nova',
+	fontStyle: 'normal',
+	fontWeight: 'normal',
+	fontSize: '40px',
+	lineHeight: '100px',
+	display: 'flex',
+	alignItems: 'center',
+	textAlign: 'center',
+	color: '#FCFCFC'
+}
+
+
+// PARA LA PARTE SUPERIOR DE INICIO
+
+
+export const titulo={
+	fontFamily: 'Ibarra Real Nova',
+	fontStyle: 'normal',
+	fontWeight: 'normal',
+	fontSize: '72px',
+	lineHeight: '90px',
+	alignItems: 'center',
+	textAlign: 'center',
+	color: '#503000'
+}
+
+export const descripcion_titulo={
+	fontFamily: 'Ibarra Real Nova',
+	fontStyle: 'normal',
+	fontWeight: 'normal',
+	fontSize: '20px',
+	lineHeight: '56px',
+	display: 'flex',
+	alignItems: 'center',
+	textAlign: 'center',
+	color: '#503000'
+}
+
+export const foto_inicio={
+	position: 'absolute',
+	width: '1440px',
+	height: '865px',
+	left: '3px',
+	top: '0px'
+}
+
+
+// CUERPO DE INICIO
+
+export const subtitulo={
+	fontFamily: 'Habibi',
+	fontStyle: 'normal',
+	fontWeight: 'normal',
+	fontSize: '50px',
+	lineHeight: '62px',
+	display: 'flex',
+	alignItems: 'center',
+	textAlign: 'center',
+	color: '#503000'
+}
+
+export const cuadrados_servicios={
+	width: '609px',
+	height: '515px',
+	left: '54px',
+	top: '1087px',
+	background: '#FFFFFF',
+	border:' 2px solid #505149',
+	boxSizing: 'border-box'
+}
+
+export const titulo_servicio={
+	fontFamily: 'Varela',
+	fontStyle: 'normal',
+	fontWeight: 'normal',
+	fontSize: '36px',
+	lineHeight: '47px',
+	display: 'flex',
+	alignItems: 'center',
+	textAlign: 'center',
+	color: '#5A5847'
+}
+
+export const saber_mas={
+	fontFamily: 'Ibarra Real Nova',
+	fontStyle: 'normal',
+	fontWeight: 'normal',
+	fontSize: '30px',
+	lineHeight: '37px',
+	display: 'flex',
+	alignItems: 'center',
+	textAlign: 'center',
+	color: '#F6FACB'
+}
+
+export const foto_servicio={
+	width: '361.1px',
+	height: '223.28px',
+	left: '83.49px',
+	top: '1189.66px'
+}
+
+export const descripcion_servicio={
+	fontFamily: 'Varela',
+	fontStyle: 'normal',
+	fontWeight: 'normal',
+	fontSize: '15px',
+	lineHeight: '19px',
+	display: 'flex',
+	alignItems: 'center',
+	textAlign: 'center',
+	color: '#000000'
+}
+
+
+// BARRA DE NAVEGACION
+
+export const fondo_barra_navegacion={
+	width: '1440px',
+	height: '109px',
+	left: '0px',
+	top: '0px',
+	background: '#51351B'
+}
+
+export const links_navegacion={
+	listStyleType:'none', 
+	margin:'1rem', 
+	padding:'5px',
+	marginLeft:'1.5em',
+	marginRight:'1.5em',
+	display:'flex', 
+	alignItems:'center',
+	justifyContent: 'center',
+	fontFamily: 'Habibi',
+	fontStyle: 'normal',
+	fontWeight: 'normal',
+	fontSize: '25px',
+	lineHeight: '62px',
+	display: 'flex',
+	alignItems: 'center',
+	textAlign: 'center',
+	color: '#51351B'
+}
+
+export const link_hoteles={
+	fontFamily: 'Habibi',
+	fontStyle: 'normal',
+	fontWeight: 'normal',
+	fontSize: '50px',
+	lineHeight: '97px',
+	display: 'flex',
+	alignItems: 'center',
+	textAlign: 'center',
+	color: '#51351B',
+}
+
+// PAGINA RESERVAS: LOS ESTILOS DE LOS TITULOS == SUBTITULO
+
+export const imagen_reserva={
+	background: 'rgba(44, 37, 19, 0.3)'
+}
+
+export const texto_reserva={
+	fontFamily: 'Habibi',
+	fontStyle: 'normal',
+	fontWeight: 'normal',
+	fontSize: '24px',
+	lineHeight: '30px',
+	display: 'flex',
+	alignItems: 'center',
+	color: '#000000'
+}
+
+export const boton_reserva={
+	fontFamily: 'Asul',
+	fontStyle: 'normal',
+	fontWeight: 'normal',
+	fontSize: '24px',
+	lineHeight: '29px',
+	display: 'flex',
+	alignItems: 'center',
+	textAlign: 'center',
+	color: '#FAF4EB'
+}
+
+export const cuadrado_reservas={
+	background: '#FCFCFC',
+	border: '1px solid #000000',
+	boxSizing: 'border-box'
+}
+
+
+// PAGINA DE SERVICIO DE HABITACIONES TITULOS == SUBTITULO
+
+export const titulo_tipo_servicio={
+	fontFamily: 'Habibi',
+	fontStyle: 'normal',
+	fontWeight: 'normal',
+	fontSize: '64px',
+	lineHeight: '80px',
+	display: 'flex',
+	alignItems: 'center',
+	textAlign: 'center',
+	color: '#000000'
+}
+
+export const texto_serv_habitaciones={
+	fontFamily: 'Habibi',
+	fontStyle: 'normal',
+	fontWeight: 'normal',
+	fontSize: '24px',
+	lineHeight: '30px',
+	display: 'flex',
+	alignItems: 'center',
+	color: '#000000'
+}
+
+
+export const estilo_input_otros_servicios={
+	background:'#FCFCFC',
+	border: '2px solid rgba(0, 0, 0, 0.7)',
+	boxSizing: 'border-box',
+	borderRadius: '4px'
+}
+
+export const boton_serv_habitaciones={
+	fontFamily: 'Asul',
+	fontStyle: 'normal',
+	fontWeight: 'normal',
+	fontSize: '24px',
+	lineHeight: '29px',
+	display: 'flex',
+	alignItems: 'center',
+	textAlign: 'center',
+	color: '#FAF4EB'
+}
+
+// SERVICIO DE TAXI/VTC...
+
+export const subtitulo_taxi={
+	fontFamily: 'Varela',
+	fontStyle: 'normal',
+	fontWeight: 'normal',
+	fontSize: '36px',
+	lineHeight: '47px',
+	display: 'flex',
+	alignItems: 'center',
+	textAlign: 'center',
+	color: '#785B2E'
+}
+
+
+// SERVICIOS DE OCIO
+
+export const links_fotos={
+	fontFamily: 'Montserrat',
+	fontStyle: 'normal',
+	fontWeight: 'bold',
+	fontSize: '48px',
+	lineHeight: '59px',
+	textAlign: 'center',
+	color: '#473D19'
+}
+
