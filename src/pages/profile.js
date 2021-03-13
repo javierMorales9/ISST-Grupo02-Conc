f376a9ad2075879a4c59a8dbdabbf08a501ccd22
acallejasz <5aadrianc@gmail.com>
2021-03-13 15:58:15 +0100

    Add_profilePage_merged_from_cozar_with_fixed_css

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,20 +1,47 @@
 import React from 'react';
-import {footer, titulo, descripcion_titulo, foto_inicio, subtitulo, cuadrados_servicios, titulo_servicio, saber_mas, foto_servicio, 
-descripcion_servicio, links_navegacion, link_hoteles, footer_upper_text, footer_lower_text, links_titulo, link_hoteles_titulo} from '../public/estilos_inicio'
-import color from '../media/color.jpeg'
-import obras from '../media/obras.png'
+import {subtitulo} from '../public/estilos_inicio'
 import NavBar from '../components/navbar.js'
 import Footer from '../components/footer.js'
+import default_profile from '../media/default_profile.jpeg'
+import '../public/profile.css';
+import Calendar from '../components/calendar.js'
 
 export default class Profile extends React.Component {
     render() {
         return (
-          <div>
-              <NavBar/>
-              <h1 style={{textAlign:'center', marginBottom:'750px'}}>Profile</h1>
-              
-              <Footer/>
-          </div>
+        	<div>
+            	<NavBar/>
+            	
+            		<h1 style={subtitulo}>Profile</h1>
+            		<hr style={{color:'gray', width: '70%', border:'2px solid'}}/>
+
+
+            		<div style={{height:'1000px',display: 'flex', flexDirection:'column', alignItems: 'center', justifyContent:'center', padding:'50px 0px 50px 0px'}}>
+            			<div style={{flex:'1', display: 'flex', flexDirection:'row', justifyContent: 'space-between', width:'950px'}}>
+            				<div style={{flex:'1'}}>
+            					<img src={default_profile} style={{}} width='525px'/>
+            				</div>
+            				<div style={{flex: '1', padding: '10px 10px 10px 80px', display: 'flex', flexDirection:'column', justifyContent:'flex-start', alignItems: 'flex-start'}}>
+	            					<span className='datos'>Nombre: Adrian Callejas Zurita</span>
+	            					<span className='datos'>email: 5acallejaszurita@gmail.com</span>
+	            					<span className='datos'>DNI: 46798359E</span>
+	            					<span className='datos'>Fecha inicio: 01/04/2021</span>
+	            					<span className='datos'>Fecha salida: 02/04/2021</span>
+	            					<span className='datos'>Metodo de pago: Visa  E20...</span>
+	            					<span className='datos'>Tipo de cliente: Premium</span>
+	            					<div style={{display: 'flex', marginTop: '15px', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start'}}>
+	            						<button className='profile_actions'><span className='text_button'>Cambiar Contraseña</span></button>
+	            						<button className='profile_actions'><span className='text_button'>Cambiar Datos</span></button>
+	            					</div>
+            				</div>
+            			</div>
+                        <div style={{flex:'1'}}>
+                            <Calendar/>
+                        </div>
+            		</div>
+
+            	<Footer/>
+          	</div>
         );
     }
 }
