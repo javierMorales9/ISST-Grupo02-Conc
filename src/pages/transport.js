4eeb9bc1a4fa541612aaa8644bac4a93075cb013
acallejasz <5aadrianc@gmail.com>
2021-03-13 18:13:32 +0100

    Merge_trasportPage_from_javi

--- src/pages/transport.js
+++ src/pages/transport.js
@@ -1,13 +1,75 @@
 import React from 'react';
 import NavBar from '../components/navbar.js';
 import Footer from '../components/footer.js';
+import MapContainer from '../components/Map';
+import '../public/transport.css';
+import '../public/main_styles.css';
 
 export default class Transport extends React.Component {
     render() {
         return (
           <div>
               <NavBar/>
-              <h1 style={{textAlign:'center', marginBottom:'750px'}}>Transport</h1>
+              <h1 className="subtitulo">Transporte</h1>
+              <hr className="raya_titulo"/>
+
+              <h2 className="category">Mis reservas</h2>
+              <div id="reseras_general_transporte"> 
+                <div id="reservas_hechas">
+                    <div id="mis_reservas">
+                            {/*Aquí habría que extraer las resrvas del servidor y hacer un map para que te las meta*/}
+                            <div className="reserva_transporte">
+                                <p>Destino:C/ de la Fresa nº19 P12 3ºD</p>
+                                <p>Hora de Recogida: 04/03/2024 12:31</p>
+                                <p>Tipo de transporte: tren</p>
+                            </div>
+                            <div className="reserva_transporte">
+                                <p>Destino:C/ de la Fresa nº19 P12 3ºD</p>
+                                <p>Hora de Recogida: 04/03/2024 12:31</p>
+                                <p>Tipo de transporte: tren</p>
+                            </div>
+                            <div className="reserva_transporte">
+                                <p>Destino:C/ de la Fresa nº19 P12 3ºD</p>
+                                <p>Hora de Recogida: 04/03/2024 12:31</p>
+                                <p>Tipo de transporte: tren</p>
+                            </div>
+                            <div className="reserva_transporte">
+                                <p>Destino:C/ de la Fresa nº19 P12 3ºD</p>
+                                <p>Hora de Recogida: 04/03/2024 12:31</p>
+                                <p>Tipo de transporte: tren</p>
+                            </div>
+                    </div>
+
+                    <div className="map">
+                        {/*para cada reserva habría que generar un pin y pasárselo como prop a MapContainer*/}
+                        <MapContainer />
+                    </div>
+                </div>
+              </div>
+              
+              <h2 className="category">Nueva Reserva</h2>
+              <div className="formulario_reserva">
+                <form>
+                    <label id="form_hab">
+                        nºhabitacion: <input type="text" name="name"/>
+                    </label>
+                    <label id="form_tipo">
+                        tipo de transporte: 
+                        <select>
+                            <option selected value="none">--None--</option>
+                            <option value="uber">Uber</option>
+                            <option value="taxi">Taxi</option>
+                        </select>
+                    </label>
+                    <label id="form_esp">
+                         Algún otro requisito especial:
+                         <br></br>
+                        <textarea></textarea>
+                    </label>
+                    <input id="form_submit" type="submit" value="Submit" />
+                </form>
+              </div>
+             
               <Footer/>
           </div>
         );
