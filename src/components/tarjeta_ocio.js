bb6b401876c960c4b357f22cbf537c169269e075
acallejasz <5aadrianc@gmail.com>
2021-03-14 19:16:17 +0100

    Added_cardFlip_ocioPage_and_Form

--- src/components/tarjeta_ocio.js
+++ src/components/tarjeta_ocio.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import '../public/leisure.css';
+import Form from '../components/form.js';
+import '../public/tarjeta_ocio.css';
+
+function rutaImg(props){
+    return '../media/' + props;
+    }
 
 export default class TarjetaOcio extends React.Component {
 	render() {
 		return (
-            <div className='leisure_tarjet' style={{backgroundImage:`url(/media/${this.props.imagen})`}}>
-                <div className='leisure_title'>
-                    <h1 className='leisure_name'>{this.props.name}</h1>
+            <div className="flip-box">
+                <div className="flip-box-inner">
+                    <div className="flip-box-front">
+                        <img src={rutaImg(this.props.imagen)} style={{width:'1000px',height:'500px'}}/>
+                        <div className='leisure_title'>
+                            <h1 className='leisure_name'>{this.props.name}</h1>
+                        </div>
+                    </div>
+                    <div className="flip-box-back">
+                        <h2 className="titulo_ocio">Nueva Reserva</h2>
+                        <Form page='leisure'/>
+                    </div>
                 </div>
             </div>
-		);
+        );    
 	}
 }
--- src/pages/transport.js
+++ src/pages/transport.js
@@ -2,19 +2,20 @@ import React from 'react';
 import NavBar from '../components/navbar.js';
 import Footer from '../components/footer.js';
 import MapContainer from '../components/Map';
+import Form from '../components/form.js';
 import '../public/transport.css';
 import '../public/main_styles.css';
 
 export default class Transport extends React.Component {
     render() {
         return (
-          <div>
-              <NavBar/>
-              <h1 className="subtitulo">Transporte</h1>
-              <hr className="raya_titulo"/>
+            <div>
+                <NavBar/>
+                <h1 className="subtitulo">Transporte</h1>
+                <hr className="raya_titulo"/>
 
-              <h2 className="category">Mis reservas</h2>
-              <div id="reseras_general_transporte"> 
+                <h2 className="category">Mis reservas</h2>
+                <div id="reseras_general_transporte"> 
                 <div id="reservas_hechas">
                     <div id="mis_reservas">
                             {/*Aquí habría que extraer las resrvas del servidor y hacer un map para que te las meta*/}
@@ -45,33 +46,14 @@ export default class Transport extends React.Component {
                         <MapContainer />
                     </div>
                 </div>
-              </div>
-              
-              <h2 className="category">Nueva Reserva</h2>
-              <div className="formulario_reserva">
-                <form>
-                    <label id="form_hab">
-                        Nº habitacion: <input type="text" name="name"/>
-                    </label>
-                    <label id="form_tipo">
-                        Tipo de transporte: 
-                        <select>
-                            <option selected value="none">--None--</option>
-                            <option value="uber">Uber</option>
-                            <option value="taxi">Taxi</option>
-                        </select>
-                    </label>
-                    <label id="form_esp">
-                         Algún otro requisito especial:
-                         <br></br>
-                        <textarea></textarea>
-                    </label>
-                    <input id="form_submit" type="submit" value="Enviar" />
-                </form>
-              </div>
-             
-              <Footer/>
-          </div>
+                </div>
+                
+                <h2 className="category">Nueva Reserva</h2>
+            
+                <Form page='transport'/>
+
+                <Footer/>
+            </div>
         );
     }
 }
