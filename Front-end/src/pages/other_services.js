826adb525137676fdb1f4ba4d69c1dde00c4899b
esmartin <enriquesalinask8@hotmail.com>
2021-04-07 09:12:26 +0200

    Corrected f5 probs (premium) n prevent not_premium_msg for appearing 2 premium usrs

--- Front-end/src/pages/other_services.js
+++ Front-end/src/pages/other_services.js
@@ -21,7 +21,8 @@ export default class RoomService extends React.Component {
 		this.state = {
 			receivedtext: "",
       upload: false,
-      tipo: ""
+      tipo: "",
+      //not_authorized_to_paint: true
 		}
 	}
 
@@ -38,7 +39,7 @@ export default class RoomService extends React.Component {
 	}
 
   async componentDidMount(){   
-
+    console.log("printed")
     if(this.props.login){
         let client = await fetch("http://localhost:8080/Concierge/rest/client/"+this.props.id_cliente,{
             method: "GET",
@@ -47,13 +48,16 @@ export default class RoomService extends React.Component {
         this.setState({tipo:client.tipo})
 
     }
+    //paint now:
+    //this.setState({not_authorized_to_paint: false})
 }
-
-	async componentDidUpdate () { 
-
-		if (this.state.upload){
+ 	async componentDidUpdate () { 
+   	if (this.state.upload || (this.state.tipo==="" && this.props.login)){
 			
 			let cliente = await fetch("http://localhost:8080/Concierge/rest/client/"+this.props.id_cliente).then(res=>res.json());
+
+      //update client - still premium?
+      this.setState({tipo:cliente.tipo})
 
 			let data = {
 				disponibilidad:true,
@@ -74,8 +78,11 @@ export default class RoomService extends React.Component {
 				body:JSON.stringify(data)
 			})
 
+
 			this.setState({upload:false, receivedtext:"" });
 		}
+    //if (this.state.tipo!="" && this.state.not_authorized_to_paint === true)
+      //        this.setState({not_authorized_to_paint: false})
 	}
 
   render() {
@@ -91,9 +98,13 @@ export default class RoomService extends React.Component {
       }
     } 
 
-    if (this.state.tipo!="Premium"){
+      console.log(this.state.tipo)
       return (
-          <div>
+        <div>
+          
+        {this.state.tipo==="" ? <div></div> : 
+        this.state.tipo!="Premium" ? 
+            <div>
                <h1 className='subtitulo'>Premium</h1>
                <hr style={{ color: 'gray', width: '70%', border: '2px solid' }} />
 
@@ -101,54 +112,59 @@ export default class RoomService extends React.Component {
                   SU NIVEL DE USUARIO NO TIENE PERMITIDO EL ACCESO A ESTE SERVICIO
               </div>
               <Footer/>
-          </div>
-      );
-    } else {
-      return (
-        <div>
-          <h1 className='subtitulo'>Premium</h1>
-          <hr style={{ color: 'gray', width: '70%', border: '2px solid' }} />
-  
-          {/*OTRAS PETICIONES*/} 
-          <div className='otros_container'>
-            <div id='id_otros'>
-            <div className='sin-background'>Peticion abierta</div>
             </div>
-  
-            <div className='formularioGrande_container'>
-              <div className='formularioSolicitud'>
-                  <div style={{display:'flex', flexDirection:'column'}}> 
-                    {t ? <div style={{display:'flex', flexDirection:'row', alignItems:'center', justifyContent: 'center'}}>
-                      <div style={{margin:'10px 0px'}}>Pulse para introducir por voz:</div>
-                      <div>
-                        <button style={{marginLeft:'10px'}} onClick={this.recordvoice}>
-                          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M16 12V6C16 3.783 14.215 1.979 12.021 1.979C11.9506 1.97942 11.8805 1.98781 11.812 2.004C10.7853 2.05378 9.81693 2.49635 9.10738 3.24015C8.39783 3.98395 8.00136 4.97204 8 6V12C8 14.206 9.794 16 12 16C14.206 16 16 14.206 16 12ZM10 12V6C10 4.897 10.897 4 12 4C12.0547 4.00002 12.1092 3.995 12.163 3.985C13.188 4.06 14 4.935 14 6V12C14 13.103 13.103 14 12 14C10.897 14 10 13.103 10 12Z" fill="black"/>
-                            <path d="M6 12H4C4 16.072 7.061 19.436 11 19.931V22H13V19.931C16.939 19.436 20 16.073 20 12H18C18 15.309 15.309 18 12 18C8.691 18 6 15.309 6 12Z" fill="black"/>
-                          </svg>
-                        </button>
-                      </div>    
-                    </div> 
-                    : <div></div>}
-                    <div style={{margin:'10px 0px'}}>Introduzca su petición aquí:</div>  
-                  </div>
-              </div>
-  
-              <form className='formularioGrande_container'>
-                <input id='inputGrande' type="text" value={this.state.receivedtext || '' } onChange = {(e)=> this.changeState(e.target.value)}/>
-                <div className='formulario'>
+
+            :
+            <div>
+              <h1 className='subtitulo'>Premium</h1>
+              <hr style={{ color: 'gray', width: '70%', border: '2px solid' }} />
+      
+              {/*OTRAS PETICIONES*/} 
+              <div className='otros_container'>
+                <div id='id_otros'>
+                <div className='sin-background'>Peticion abierta</div>
                 </div>
-              </form>
-  
-              <button id="form_submit" onClick={()=>this.setState({upload:true})}>Enviar</button>
-              
-            </div>
-          </div>
-          <Footer/>
+      
+                <div className='formularioGrande_container'>
+                  <div className='formularioSolicitud'>
+                      <div style={{display:'flex', flexDirection:'column'}}> 
+                        {t ? <div style={{display:'flex', flexDirection:'row', alignItems:'center', justifyContent: 'center'}}>
+                          <div style={{margin:'10px 0px'}}>Pulse para introducir por voz:</div>
+                          <div>
+                            <button style={{marginLeft:'10px'}} onClick={this.recordvoice}>
+                              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M16 12V6C16 3.783 14.215 1.979 12.021 1.979C11.9506 1.97942 11.8805 1.98781 11.812 2.004C10.7853 2.05378 9.81693 2.49635 9.10738 3.24015C8.39783 3.98395 8.00136 4.97204 8 6V12C8 14.206 9.794 16 12 16C14.206 16 16 14.206 16 12ZM10 12V6C10 4.897 10.897 4 12 4C12.0547 4.00002 12.1092 3.995 12.163 3.985C13.188 4.06 14 4.935 14 6V12C14 13.103 13.103 14 12 14C10.897 14 10 13.103 10 12Z" fill="black"/>
+                                <path d="M6 12H4C4 16.072 7.061 19.436 11 19.931V22H13V19.931C16.939 19.436 20 16.073 20 12H18C18 15.309 15.309 18 12 18C8.691 18 6 15.309 6 12Z" fill="black"/>
+                              </svg>
+                            </button>
+                          </div>    
+                        </div> 
+                        : <div></div>}
+                        <div style={{margin:'10px 0px'}}>Introduzca su petición aquí:</div>  
+                      </div>
+                  </div>
+      
+                  <form className='formularioGrande_container'>
+                    <input id='inputGrande' type="text" value={this.state.receivedtext || '' } onChange = {(e)=> this.changeState(e.target.value)}/>
+                    <div className='formulario'>
+                    </div>
+                  </form>
+      
+                  <button id="form_submit" onClick={()=>this.setState({upload:true})}>Enviar</button>
+                  
+                </div>
+              </div>
+              <Footer/>
           
-        </div>
+            </div>
+      
+      }
+      </div>
+        
       );
-    }
+    
+    
+  //envuelve hasta aquí
   }
 }
 
